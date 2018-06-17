268f44200a5f12f55e132785f2081343c4c4363f
dauss75 <dauss75@gmail.com>
2018-06-17 17:07:43 -0500

    script update

--- script/maf_data_process.R
+++ script/maf_data_process.R
@@ -1,74 +1,32 @@
-maf_to_matrix<-function(maf_dir_path, clinical_data, data){
+maf_to_matrix<-function(maf_dir_path, clinical_data, data_matrix){
   # data_preprocessing(raw_dir_path, clinical_data, data)
-  dir.create(data, showWarnings = FALSE)
+  #data_matrix<-file.path(data, "gdc_tcga")
+  #data_matrix<-data
+  dir.create(data_matrix,showWarnings = FALSE)
   maf_files<-list.files(maf_dir_path, full.names = TRUE)
   
   clinical<-fread(clinical_data)
   colnames(clinical)[2]<-"Tumor_Sample_Barcode"
   
   # create an aggregated data matrix
-  y_cnt<-NULL; y_aa_mis<-NULL; y_aa_all<-NULL; 
-  x_cnt<-NULL; x_aa_mis<-NULL; x_aa_all<-NULL;
-  y_map<-NULL; y_summary<-NULL; tcga_type<-NULL; 
+  y<-NULL; y_sample<-NULL
   
   for (i in 1:length(maf_files)){
-    # sink(file=paste(data,"/",basename(maf_files[i]),"_summary.txt",sep=""))
     laml<-read.maf(maf=maf_files[i],isTCGA = FALSE, clinicalData=clinical,removeDuplicatedVariants=F)
-    # sink()
-    
-    mut_cnt<-mutCountMatrix(maf=laml, includeSyn=F)
-    # x_cnt<-merge(x_cnt,mut_cnt, by="row.names",all=TRUE)
-    # rownames(x_cnt)<-x_cnt$Row.names; x_cnt$Row.names<-NULL
-    y_cnt<-c(y_cnt,rep(i-1,ncol(mut_cnt)))
-    # y_map<-rbind(y_map,paste(i,unlist(strsplit(basename(maf_files[i]),"[.]"))[2],sep="\t"))
-    # y_summary<-rbind(y_summary,paste(nrow(mut_cnt),ncol(mut_cnt),sep="\t"))
-    
-    # png(file = paste("/Users/sjung/Documents/GitHub/gdc_snp/figs/",unlist(strsplit(basename(maf_files[i]),"[.]"))[2],".png",sep=""), width = 1024, height = 768)
-    # plotmafSummary(maf = laml, rmOutlier = TRUE, addStat = 'median', dashboard = TRUE, titvRaw = FALSE, titleSize = c(14,12), fs=14, statFontSize=5)
-    # dev.off()
-    
-    # laml@data[["Hugo_Symbol"]]<-paste(laml@data[["Hugo_Symbol"]],":",laml@data[["Protein_Change"]],sep="")
+    snp_cnt<-mutCountMatrix(maf=laml, includeSyn=FALSE, countOnly = "Missense_Mutation")
     laml@data[["Hugo_Symbol"]]<-paste(laml@data[["Hugo_Symbol"]],":",laml@data[["HGVSp_Short"]],sep="")
-    #snp_aa_all<-mutCountMatrix(laml, includeSyn = FALSE)
-    #x_aa_all<-merge(x_aa_all,snp_aa_all, by="row.names",all=TRUE)
-    #rownames(x_aa_all)<-x_aa_all$Row.names; x_aa_all$Row.names<-NULL
-    #y_aa_all<-c(y_aa_all,rep(i-1,ncol(snp_aa_all)))
+    snp_aa<-mutCountMatrix(laml, includeSyn = FALSE, countOnly = "Missense_Mutation")
+    y<-c(y,rep(i-1,ncol(snp_aa))); y_sample<-c(y_sample,colnames(snp_aa))
     
-    snp_aa_mis<-mutCountMatrix(laml, includeSyn = FALSE, countOnly = "Missense_Mutation")
-    # x_aa_mis<-merge(x_aa_mis,snp_aa_mis, by="row.names",all=TRUE)
-    # rownames(x_aa_mis)<-x_aa_mis$Row.names; x_aa_mis$Row.names<-NULL
-    y_aa_mis<-c(y_aa_mis,rep(i-1,ncol(snp_aa_mis)))
-    
-    write.table(mut_cnt,paste(data,"/",basename(maf_files[i]),"_mut_cnt",sep=""),row.names = TRUE, col.names=TRUE,quote = FALSE)
-    fwrite(mut_cnt,paste(data,"/",basename(maf_files[i]),"mut_cnt",sep=""),row.names = TRUE, col.names=TRUE)
-    write.table(snp_aa_mis,paste(data,"/",basename(maf_files[i]),".snp_aa",sep=""),row.names = TRUE, col.names=TRUE,quote = FALSE)
-    fwrite(snp_aa_mis,paste(data,"/",basename(maf_files[i]),"_snp_aa",sep=""),row.names = TRUE, col.names=TRUE)
-    
-    # rm(laml)
-    
-    # sprintf("%f is done", i)
+    sample_name<-paste(unlist(strsplit(basename(maf_files[i]),'[.]'))[1:2],collapse="-")
+    fwrite(snp_cnt,paste(data_matrix,"/",sample_name,".snp_cnt.txt",sep=""),row.names = TRUE, col.names=TRUE)
+    fwrite(snp_aa,paste(data_matrix,"/",sample_name,".snp_gene_aa.txt",sep=""),row.names = TRUE, col.names=TRUE)
+    rm(laml,snp_cnt,snp_aa)
   }
-  # rm(snp_aa_mis, mut_cnt,laml,clinical)
-  # write.table(rownames(x),paste(data,"/mut_gene_list.txt",sep=""),row.names = FALSE, col.names=FALSE,quote = FALSE)
-  # write.table(colnames(x),paste(data,"/mut_sample_list.txt",sep=""),row.names = FALSE, col.names=FALSE,quote = FALSE)
-  # save(x,y_label,y_map,y_summary,file=paste(data,"/combined_mut_data.RData",sep=""))
-  # x_cnt[is.na(x_cnt)] <- 0; x_aa_mis[is.na(x_aa_mis)] <- 0; #x_aa_all[is.na(x_aa_all)] <- 0; 
-  
-  # x_cnt<-as.data.frame(t(x_cnt)); x_aa_mis<-as.data.frame(t(x_aa_mis)); #x_aa_all<-as.data.frame(t(x_aa_all))
-  y_cnt<-as.data.frame(y_cnt); y_aa_mis<-as.data.frame(y_aa_mis); #y_aa_all<-as.data.frame(y_aa_all)
-  # write.table(x_cnt,paste(data,"/x_cnt",sep=""),row.names = TRUE, col.names=TRUE,quote = FALSE)
-  # fwrite(x_cnt,paste(data,"/x_cnt_2",sep=""),sep=" ", row.names=T, col.names = T)
-  # write.table(x_aa_mis,paste(data,"/x_aa_mis",sep=""),row.names = TRUE, col.names=TRUE,quote = FALSE)
-  # fwrite(x_aa_mis,paste(data,"/x_aa_mis_2",sep=""),sep=" ", row.names=T, col.names = T)
-  #write.table(x_aa_all,paste(data,"/x_aa_all",sep=""),row.names = TRUE, col.names=TRUE,quote = FALSE)
-  
-  write.table(y_cnt,paste(data,"/y_cnt_mis",sep=""),row.names = FALSE, col.names=FALSE,quote = FALSE)
-  write.table(y_aa_mis,paste(data,"/y_aa_mis",sep=""),row.names = FALSE, col.names=FALSE,quote = FALSE)
-  #write.table(y_aa_all,paste(data,"/y_aa_all",sep=""),row.names = FALSE, col.names=FALSE,quote = FALSE)
-  # write.table(y_map,paste(data,"/y_mut_count.map",sep=""),row.names = FALSE, col.names=FALSE,quote = FALSE)
-  # write.table(y_summary,paste(data,"/y_mut_count.summary",sep=""),row.names = FALSE, col.names=FALSE,quote = FALSE)
-} 
-
+  y<-as.data.frame(y); y_sample<-as.data.frame(y_sample)
+  fwrite(y,paste(data_matrix,"/y_label.txt",sep=""),row.names = FALSE, col.names=FALSE,quote = FALSE)
+  fwrite(y_sample,paste(data_matrix,"/y_samplename.txt",sep=""),row.names = FALSE, col.names=FALSE,quote = FALSE)
+}
 
 gene_devide_exon_size<-function(exprs){
   txdb <- TxDb.Hsapiens.UCSC.hg38.knownGene
@@ -79,26 +37,105 @@ gene_devide_exon_size<-function(exprs){
   exonic.gene.sizes<-na.omit(exonic.gene.sizes)
   rownames(exonic.gene.sizes)<-exonic.gene.sizes$genesymbol
   
-  y<-as.data.frame(exprs[,1]); x<-as.data.frame(t(exprs[,-1]))
+  # y<-as.data.frame(exprs[,1]); 
+  x<-as.data.frame(exprs); rownames(x)<-rownames(exprs)
   
-  # retrieve pathway
   com_gene<-intersect(rownames(x),rownames(exonic.gene.sizes))
   x2<-x[com_gene,]; x2<-na.omit(x2)
-  exonic.gene.sizes2<-exonic.gene.sizes[com_gene,]
+  exonic.gene.sizes2<-exonic.gene.sizes[com_gene,] # sort by the common gene
   exprs_devided_by_exon_size<-x2/exonic.gene.sizes2$size
   exprs_devided_by_exon_size
 }
 
-combine_matrix<-function(data_path){
-#  cnt_files<-list.files(data_path, pattern = "_cnt$", full.names = TRUE)
-#  cnt_data <- Reduce(function(x, y) merge(x, y, all=TRUE), lapply(cnt_files,fread)); row.names(cnt_data)<-cnt_data$V1;cnt_data$V1<-NULL 
-#  cnt_data[is.na(cnt_data)] <- 0
-#  fwrite(cnt_data,paste(data,"/cnt_merged_data",sep=""),sep=" ", row.names=T, col.names = T);
-#  rm(cnt_data)
-  aa_files<-list.files(data_path, pattern = "_aa$", full.names = TRUE)
-  aa_data2 <- Reduce(function(x, y) merge(x, y, all=TRUE), lapply(aa_files,fread)); 
-  row.names(aa_data)<-aa_data$V1;
-  aa_data$V1<-NULL 
-  aa_data[is.na(aa_data)] <- 0; 
-  fwrite(aa_data,paste(data,"/aa_merged_data",sep=""),sep=" ", row.names=T, col.names = T)
+combine_matrix<-function(data_path, gene=NULL){
+  library(reshape2)
+  snp_cnt_files<-list.files(data_path, pattern = "_cnt.txt$", full.names = TRUE)
+  y_label<-fread(paste(data_path, "/y_label.txt",sep=""))
+  if (is.null(gene)){
+    snp_cnt_data <- Reduce(function(x, y) merge(x, y, all=TRUE), lapply(snp_cnt_files,fread)); 
+    row.names(snp_cnt_data)<-snp_cnt_data$V1;snp_cnt_data$V1<-NULL
+    snp_cnt_data[is.na(snp_cnt_data)] <- 0
+    fwrite(snp_cnt_data,paste(data,"/snp_cnt_merged.txt",sep=""), row.names=T, col.names = T);
+  } else {
+    gene_list<-fread(gene); 
+    snp_cnt_data<-gene_list
+    for (i in 1:length(snp_cnt_files)){
+      t_data<-as.data.frame(fread(snp_cnt_files[i])); rownames(t_data)<-t_data$V1; t_data$V1<-NULL
+      t_data2<-t_data[gene_list$HGNC_SYMBOL,]; t_data2[is.na(t_data2)] <- 0
+      snp_cnt_data<-cbind(snp_cnt_data,t_data2)
+    }
+    rm(t_data,t_data2)
+    rownames(snp_cnt_data)<-snp_cnt_data$HGNC_SYMBOL;snp_cnt_data$HGNC_SYMBOL<-NULL
+    
+    snp_cnt_data2<-snp_cnt_data[rowSums(snp_cnt_data)!=0,];
+    snp_cnt_data2<-as.data.frame(snp_cnt_data2)
+    rownames(snp_cnt_data2)<-rownames(snp_cnt_data)[which(rowSums(snp_cnt_data)!=0)];
+    snp_cnt_data3<-snp_cnt_data2[,colSums(snp_cnt_data2) != 0];
+    y_label2<-y_label[colSums(snp_cnt_data2)!=0,]
+    rownames(y_label2)<-colnames(snp_cnt_data3)
+    fwrite(snp_cnt_data3,paste(data,"/",unlist(strsplit(basename(gene),"[.]"))[1],".snp_cnt_merged.txt",sep=""), row.names=T, col.names = T);
+    fwrite(y_label2,paste(data,"/",unlist(strsplit(basename(gene),"[.]"))[1],".snp_cnt_merged_label.txt",sep=""), row.names=T, col.names = T);
+  }
+  rm(snp_cnt_data,snp_cnt_data2,snp_cnt_data3,y_label,y_label2)
+  
+  snp_aa_files<-list.files(data_path, pattern = "_aa.txt$", full.names = TRUE)
+  y_label<-fread(paste(data_path, "/y_label.txt",sep=""))
+  if (is.null(gene)){
+    snp_aa_data <- Reduce(function(x, y) merge(x, y, all=TRUE), lapply(snp_aa_files,fread)); 
+    row.names(snp_aa_data)<-snp_aa_data$V1;snp_aa_data$V1<-NULL
+    snp_aa_data[is.na(snp_aa_data)] <- 0
+    fwrite(snp_cnt_data,paste(data,"/snp_aa_merged.txt",sep=""), row.names=T, col.names = T);
+  } else {
+    gene_list<-fread(gene); 
+    snp_aa_data<-gene_list;
+    x<-NULL
+    for (i in 1:length(snp_aa_files)){
+    # for (i in 1:3){
+      t_data<-as.data.frame(fread(snp_aa_files[i])); rownames(t_data)<-t_data$V1; 
+      t_data$V1<-colsplit(string=t_data$V1, pattern=":", names=c("Gene", "AA"))[,1]
+      tmp<-t_data[which(t_data$V1 %in% gene_list$HGNC_SYMBOL),];tmp$V1<-NULL
+      x<-merge(x,tmp,by="row.names",all=TRUE)
+      rownames(x)<-x$Row.names; x$Row.names<-NULL
+      x[is.na(x)] <- 0
+      print(i)
+    }
+    rm(t_data,tmp,snp_aa_data)
+    snp_aa_data<-x;
+    snp_aa_data2<-snp_aa_data[rowSums(snp_aa_data)!=0,]
+    snp_aa_data3<-snp_aa_data2[,colSums(snp_aa_data2) != 0];
+    y_label2<-y_label[which(colSums(snp_aa_data2)!=0),]
+    rownames(y_label2)<-colnames(snp_aa_data3)
+    fwrite(snp_aa_data3,paste(data,"/",unlist(strsplit(basename(gene),"[.]"))[1],".snp_aa_merged.txt",sep=""), row.names=T, col.names = T);
+    fwrite(y_label2,paste(data,"/",unlist(strsplit(basename(gene),"[.]"))[1],".snp_aa_merged_label.txt",sep=""), row.names=T, col.names = T);
+  }
+  rm(snp_aa_data,snp_aa_data2,snp_aa_data3,y_label,y_label2,x)
+}
+
+#------------------------------
+#   build a pathway matrix (c2)
+#------------------------------
+build_pathway_matrix <- function(adj_exprs, p_file, y_label){
+  library(qusage)
+  library (plyr)
+  pathway<-read.gmt(p_file)
+  # pathways<-do.call(c, list(c2_pathway, c5_pathway))
+
+  df2 <- ldply (pathway, data.frame)
+  colnames(df2)<-c("pathway","gene")
+  reshape_pathway <- dcast(df2, pathway ~ gene, function(x) 1, fill = 0)
+  rownames(reshape_pathway)<-reshape_pathway$pathway; reshape_pathway$pathway<-NULL
+  
+  com_pathway_gene<-intersect(rownames(adj_exprs),colnames(reshape_pathway))
+  genes_pathway<-as.matrix(t(adj_exprs[com_pathway_gene,]))
+  reshape_pathway2<-as.matrix(t(reshape_pathway[,com_pathway_gene]))
+  
+  pathway_mutation<-genes_pathway %*% reshape_pathway2; dim(pathway_mutation)
+  
+  pathway_mutation2<-pathway_mutation[rowSums(pathway_mutation)!=0,]; dim(pathway_mutation2)
+  y_adjusted<-y_adjusted[rowSums(pathway_mutation)!=0,]
+  pathway_mutation2<-as.data.frame(pathway_mutation2[,colSums(pathway_mutation2)!=0]); dim(pathway_mutation2)
+  
+  
+  fwrite(pathway_mutation2, paste(data,"/pathway_mutation_data.txt",sep=""), row.names = TRUE)
+  fwrite(y_adjusted, paste(data,"/pathway_mutation_data_label.txt",sep=""), row.names = TRUE)
 }
--- script/main_run.R
+++ script/main_run.R
@@ -1,14 +1,66 @@
+## Rscript script/main_run.R -m input/maf_files -c input/clinical.tsv -d data/gdc_tcga -g 
+
+suppressPackageStartupMessages(library(optparse))
+
 source("/Users/sjung/Documents/GitHub/gdc_snp/script/library.R")
 
 # file and directory path
-raw_dir_path="/Users/sjung/Documents/GitHub/gdc_snp/input/maf_files"
-clinical_data<-"/Users/sjung/Documents/GitHub/gdc_snp/input/clinical.tsv"
-data="/Users/sjung/Documents/GitHub/gdc_snp/data"
-figs="/Users/sjung/Documents/GitHub/gdc_snp/figs"
+option_list = list(
+  make_option(c("-m", "--maf_dir"),  type="character", help="maf file directory path"),
+  make_option(c("-c", "--clinical"),  type="character", help="clinical data"),
+  make_option(c("-d", "--data"),  type="character", help="data directory path"),
+  make_option(c("-g", "--gene"),  type="character", help="gene list")
+  make_option(c("-p", "--pathway"),  type="character", help="pathway file")
+)
+
+opt_parser = OptionParser(option_list=option_list);
+opt = parse_args(OptionParser(option_list=option_list))
+
+if ( is.null(opt$m) & is.null(opt$c) & is.null(opt$d)) {
+    print_help(opt_parser)
+    stop("Missing input files path!\n")
+} else {
+    maf_dir_path<-opt$m;
+    clinical_data<-opt$c;
+    data<-opt$d;
+    print(data)
+}
+
+# maf_dir_path="/Users/sjung/Documents/GitHub/gdc_snp/input/maf_files"
+# clinical_data<-"/Users/sjung/Documents/GitHub/gdc_snp/input/clinical.tsv"
+# data="/Users/sjung/Documents/GitHub/gdc_snp/data/gdc_tcga"
+# gene<-"/Users/sjung/Documents/GitHub/gdc_snp/data/cosmic_ostlund_cancer_gene.csv"
 
 ## data preprocessing
 source("/Users/sjung/Documents/GitHub/gdc_snp/script/maf_data_process.R")
 
 ## main
-maf_to_matrix(raw_dir_path, clinical_data, data)
-# combine_matrix(data)
+maf_to_matrix(maf_dir_path, clinical_data, data)
+if ( is.null(opt$g)){
+  combine_matrix(data)
+} else {
+  combine_matrix(data,opt$g) #combine only the provided genes
+}
+
+# normalize snp count by gene length
+
+input_data_matrix<-paste(data,"/",unlist(strsplit(basename(opt$g),"[.]"))[1],".snp_cnt_merged.txt",sep="")
+input_data_label<-paste(data,"/",unlist(strsplit(basename(opt$g),"[.]"))[1],".snp_cnt_merged_label.txt",sep="")
+# input_data_matrix<-fread("/Users/sjung/Documents/GitHub/gdc_snp/data/gdc_tcga/cosmic_ostlund_cancer_gene.snp_cnt_merged.txt")
+# input_data_label<-fread("/Users/sjung/Documents/GitHub/gdc_snp/data/gdc_tcga/cosmic_ostlund_cancer_gene.snp_cnt_merged_label.txt")
+rownames(input_data_label)<-input_data_label$V1; input_data_label$V1<-NULL
+rownames(input_data_matrix)<-input_data_matrix$V1;input_data_matrix$V1<-NULL
+norm_gene_count<-gene_devide_exon_size(input_data_matrix)
+
+norm_gene_count_label<-input_data_label[colSums(norm_gene_count)!=0,]
+norm_gene_count<-norm_gene_count[,colSums(norm_gene_count)!=0]
+rownames(norm_gene_count_label)<-colnames(norm_gene_count)
+
+
+fwrite(norm_gene_count,paste(data,"/",unlist(strsplit(basename(opt$g),"[.]"))[1],".snp_cnt_merged_normalized.txt",sep=""), row.names = TRUE)
+fwrite(norm_gene_count_label,paste(data,"/",unlist(strsplit(basename(opt$g),"[.]"))[1],".snp_cnt_merged_normalized_label.txt",sep=""), row.names = TRUE)
+# fwrite(norm_gene_count,"/Users/sjung/Documents/GitHub/gdc_snp/data/gdc_tcga/cosmic_ostlund_cancer_gene.snp_cnt_merged_normalized.txt", row.names = TRUE)
+# fwrite(norm_gene_count_label,"/Users/sjung/Documents/GitHub/gdc_snp/data/gdc_tcga/cosmic_ostlund_cancer_gene.snp_cnt_merged_normalized_label.txt", row.names = TRUE)
+
+# calcuate pathway
+build_pathway_matrix(norm_gene_count, opt$p, norm_gene_count_label)
